3a6be5a669b6afc526596efcc1b94aed9a588672
Nargo <58916643+nargo0o@users.noreply.github.com>
2021-02-13 23:01:26 +0300

    Lesson 3 (#3)
    * Lesson_2. Templates
    
    * Lesson_2. Context for templates
    
    * Lesson_2. Repeat and filters
    
    * bug fix
    
    * Lesson 3. Django ORM start
    
    * Lesson 3. Djagno admin
    
    * Lesson 3. Models in context
    
    * Lesson 3. Urls for app
    
    * Lesson 4. Data from files
    
    * Lesson 4. Data from files
    
    * Lesson 4. Auth app and user model
    
    * Lesson 4. User login and logout
    
    * Lesson 4. User CRUD

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -38,7 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mainapp',
+    'authnapp',
 ]
+
+# Auth model
+AUTH_USER_MODEL = 'authnapp.ShopUser'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -124,3 +129,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
+# Media files
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
