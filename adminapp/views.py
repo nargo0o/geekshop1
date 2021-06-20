f19e76e6428bf2c5b88cf2adc5609f582dda8a67
Nargo <58916643+nargo0o@users.noreply.github.com>
2021-06-20 11:45:26 -0700

    Lesson 7 (#8)
    * Lesson_2. Templates
    
    * Lesson_2. Context for templates
    
    * Lesson_2. Repeat and filters
    
    * bug fix
    
    * Lesson 3. Django ORM start
    
    * Lesson 3. Djagno admin
    
    * Lesson 3. Models in context
    
    * Lesson 3. Urls for app
    
    * Lesson 4. Data from files
    
    * Lesson 4. Data from files
    
    * Lesson 4. Auth app and user model
    
    * Lesson 4. User login and logout
    
    * Lesson 4. User CRUD
    
    * Lesson 4. Data from files
    
    * Lesson 4. User login and logout
    
    * Lesson 5. Code style fix
    
    * Lesson 5. ORM filters
    
    * Lesson 5. Basket app start
    
    * Lesson 5. Basket counter
    
    * Lesson_5. Bug fixed
    
    * Lesson 6. Basket extension
    
    * Lesson 6. Hot product
    
    * Lesson 6. Basket read and delete
    
    * Lesson 6. Product page
    
    * Lesson 6. Login fix
    
    * Lesson 6. After login
    
    * Lesson 6. AJAX
    
    * lesson_7. Adminapp.
    
    * lesson_7. Admin link and firewall.
    
    * lesson_7. CRUD users.
    
    * lesson_7. CRUD categories.
    
    * Sort imports and reformat

---
+++ adminapp/views.py
@@ -0,0 +1,125 @@
+from django.conf import settings
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import HttpResponseRedirect, get_object_or_404, redirect, render
+from django.urls import reverse
+
+from adminapp.forms import ShopUserAdminEditForm
+from authnapp.forms import ShopUserRegisterForm
+from authnapp.models import ShopUser
+from mainapp.models import Product, ProductCategory
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def admin_main(request):
+    response = redirect("admin:users")
+    return response
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def users(request):
+    title = "админка/пользователи"
+    users_list = ShopUser.objects.all().order_by("-is_active", "-is_superuser", "-is_staff", "username")
+    content = {"title": title, "objects": users_list, "media_url": settings.MEDIA_URL}
+    return render(request, "adminapp/users.html", content)
+
+
+def user_create(request):
+    title = "пользователи/создание"
+
+    if request.method == "POST":
+        user_form = ShopUserRegisterForm(request.POST, request.FILES)
+        if user_form.is_valid():
+            user_form.save()
+            return HttpResponseRedirect(reverse("admin:users"))
+    else:
+        user_form = ShopUserRegisterForm()
+
+    content = {"title": title, "update_form": user_form, "media_url": settings.MEDIA_URL}
+
+    return render(request, "adminapp/user_update.html", content)
+
+
+def user_update(request, pk):
+    title = "пользователи/редактирование"
+
+    edit_user = get_object_or_404(ShopUser, pk=pk)
+    if request.method == "POST":
+        edit_form = ShopUserAdminEditForm(request.POST, request.FILES, instance=edit_user)
+        if edit_form.is_valid():
+            edit_form.save()
+            return HttpResponseRedirect(reverse("admin:user_update", args=[edit_user.pk]))
+    else:
+        edit_form = ShopUserAdminEditForm(instance=edit_user)
+
+    content = {"title": title, "update_form": edit_form, "media_url": settings.MEDIA_URL}
+
+    return render(request, "adminapp/user_update.html", content)
+
+
+def user_delete(request, pk):
+    title = "пользователи/удаление"
+
+    user = get_object_or_404(ShopUser, pk=pk)
+
+    if request.method == "POST":
+        # user.delete()
+        # Instead delete we will set users inactive
+        user.is_active = False
+        user.save()
+        return HttpResponseRedirect(reverse("admin:users"))
+
+    content = {"title": title, "user_to_delete": user, "media_url": settings.MEDIA_URL}
+
+    return render(request, "adminapp/user_delete.html", content)
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def categories(request):
+    title = "админка/категории"
+    categories_list = ProductCategory.objects.all()
+    content = {"title": title, "objects": categories_list, "media_url": settings.MEDIA_URL}
+    return render(request, "adminapp/categories.html", content)
+
+
+def category_create(request):
+    response = redirect("admin:categories")
+    return response
+
+
+def category_update(request, pk):
+    response = redirect("admin:categories")
+    return response
+
+
+def category_delete(request, pk):
+    response = redirect("admin:categories")
+    return response
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def products(request, pk):
+    title = "админка/продукт"
+    category = get_object_or_404(ProductCategory, pk=pk)
+    products_list = Product.objects.filter(category__pk=pk).order_by("name")
+    content = {"title": title, "category": category, "objects": products_list, "media_url": settings.MEDIA_URL}
+    return render(request, "adminapp/products.html", content)
+
+
+def product_create(request, pk):
+    response = redirect("admin:categories")
+    return response
+
+
+def product_read(request, pk):
+    response = redirect("admin:categories")
+    return response
+
+
+def product_update(request, pk):
+    response = redirect("admin:categories")
+    return response
+
+
+def product_delete(request, pk):
+    response = redirect("admin:categories")
+    return response
